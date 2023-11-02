6459df9c9460c93dfc15ab84f4e458d6b6956797
Derek Kershner <dkershner@dkershner.com>
2023-11-02 16:45:42 -0700

    feat: Initial Implementation

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,17 +1,23 @@
-import { awscdk } from 'projen';
+import { awscdk } from "projen";
+
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'Derek Kershner',
-  authorAddress: 'dkershner@dkershner.com',
-  cdkVersion: '2.1.0',
-  defaultReleaseBranch: 'main',
-  jsiiVersion: '~5.0.0',
-  name: 'cdk-versioned-stack-manager',
+  author: "Derek Kershner",
+  authorAddress: "https://dkershner.com",
+  cdkVersion: "2.101.1",
+  defaultReleaseBranch: "main",
+  jsiiVersion: "~5.0.0",
+  name: "cdk-versioned-stack-manager",
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/dkershner6/cdk-versioned-stack-manager.git',
+  repositoryUrl:
+    "https://github.com/dkershner6/cdk-versioned-stack-manager.git",
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  devDeps: ["@types/aws-lambda"] /* Build dependencies for this module. */,
+  bundledDeps: ["@aws-sdk/client-cloudformation"],
+  packageName: "cdk-versioned-stack-manager" /* The "name" in package.json. */,
+
+  prettier: true,
 });
+
 project.synth();
